36607f0e0f71ef93d183550ef3f3186b19ea43b7
shilpi68 <Shilpiroy2356@gmail.com>
2024-08-06 16:21:00 +0600

    projcet setting

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {RouterProvider} from "react-router-dom";
+import { router } from './Routres/Routers';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+   <div className='max-w-6xl mx-auto'>
+   <RouterProvider router={router} />
+   </div>
+  </React.StrictMode>,
+)
